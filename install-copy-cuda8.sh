cd3de44a698a0733ac58e3bd2b8601ef8de12e58
Jun Lu <jun.lu.locky@gmail.com>
2017-12-01 16:29:28 +0100

    Update install-copy-cuda8.sh

--- install-copy-cuda8.sh
+++ install-copy-cuda8.sh
@@ -18,3 +18,7 @@ tar -xzvf ${CUDNN_TAR_FILE}
 sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
 sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
 sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+
+#64 bit check info: http://docs.nvidia.com/cuda/cuda-installation-guide-linux/#runfile-uninstallation
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
